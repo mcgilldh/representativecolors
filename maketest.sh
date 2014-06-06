51d74a1a5ecc0791ad374f281fe8e099d2416717
Peter Davoust <peter@peterdavoust.com>
2014-06-06 10:33:50 -0400

    Added .idea directory so the project imports properly. Removed vestigial files that have nothing to do with the current version of the project.

--- maketest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-[[ -e colors.html ]] && rm colors.html
-for i in $(cat colors.csv); do echo '<div class="foo" style="background-color:rgb('$i')"></div>' >> colors.html; done
-cat testheader.html colors.html > fulltest.html
-echo '</body></html>' >> fulltest.html
